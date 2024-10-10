62661a55e68a37195f1296e08616c9ff21577544
hamseokwon <hamseokwon@hamseokwons-MacBook-Pro.local>
2024-10-10 15:51:39 +0900

    has and invalid with tailwind

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,20 @@
 
 export default function Home() {
   return (
-    <main className="bg-gray-100 h-screen flex items-center justify-center p-5">
-      <div className="bg-white w-full shadow-lg p-5 rounded-3xl max-w-screen-sm flex flex-col gap-2">
-        <input type="text" placeholder="Search here..." className="w-full rounded-full h-12 bg-gray-200 pl-5 outline-none ring ring-transparent focus:ring-orange-500 focus:ring-offset-2 transition-shadow placeholder:drop-shadow"/>
-        <button className="bg-black text-white py-2 rounded-full active:scale-90 transition-transform font-medium outline-none">Search</button>
+    <main className="bg-gray-100 h-screen sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 2xl:bg-purple-100 flex items-center justify-center p-5">
+      <div className="bg-white w-full shadow-lg p-5 rounded-3xl max-w-screen-sm flex flex-col md:flex-row gap-2 *:outline-none ring ring-transparent transition-shadow has-[:invalid]:ring-red-100">
+        <input
+          type="email"
+          placeholder="Email Address"
+          required
+          className="w-full rounded-full h-12 bg-gray-200 pl-5 ring ring-transparent focus:ring-purple-400 focus:ring-offset-1 transition-shadow placeholder:drop-shadow invalid:focus:ring-red-500 peer"
+        />
+        <span className="text-red-500 font-medium hidden peer-invalid:block">
+          Email is required
+        </span>
+        <button className="bg-black text-white py-2 rounded-full active:scale-90 transition-transform font-medium md:px-10 ">
+          Log In
+        </button>
       </div>
     </main>
   );
